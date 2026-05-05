15d2df16d94d174b38fbe49caba08c351b37bbbc
jorge <jorge@dominio.com>
2026-05-05 11:11:11 +0200

    Instant, Period y Duration

--- learning_java_language/src/main/java/com/ejemplo/App.java
+++ learning_java_language/src/main/java/com/ejemplo/App.java
@@ -2,12 +2,14 @@ package com.ejemplo;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.Period;
 import java.time.format.TextStyle;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjusters;
 import java.util.Locale;
 
 public class App {
+
     public static void main(String[] args) {
         LocalDate today = LocalDate.now();
         System.out.println("Hoy es " + today);
@@ -15,8 +17,17 @@ public class App {
         System.out.println("Fecha del cobro " + cobro);
         LocalDate fecha = LocalDate.of(1963, Month.JANUARY, 22).plusYears(1);
         System.out.println(fecha.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.FRANCE));
-        // Locale locale = Locale.of("ja", "JP");
-        // System.out.println(fecha.getMonth().getDisplayName(TextStyle.FULL, locale));
+        Locale locale = Locale.of("ja", "JP");
+        System.out.println(fecha.getMonth().getDisplayName(TextStyle.FULL, locale));
         System.out.println(ChronoUnit.YEARS.between(fecha, today));
+        LocalDate proximo = fecha.withYear(today.getYear());
+        if (proximo.isBefore(today) || proximo.isEqual(today)) {
+            proximo = proximo.plusYears(1);
+        }
+        Period p1 = Period.between(today, proximo);
+        long p2 = ChronoUnit.DAYS.between(today, proximo);
+        System.out.println("There are " + p1.getMonths() + " months, and "
+                + p1.getDays() + " days until your next birthday. ("
+                + p2 + " total)");
     }
 }
